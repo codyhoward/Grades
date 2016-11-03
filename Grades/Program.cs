bbb951f083dbd38d40350fd7e05e5e0f817457f0
Cody <Cody@CSCPS>
2016-11-02 21:37:44 -0400

    Distinguishing between methods via use of parameters. S6C2:Methods

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -14,16 +14,25 @@ namespace Grades
 
 
 
-            GradeBook book = new GradeBook(); 
+            GradeBook book = new GradeBook();
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
 
             GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine(stats.AverageGrade);
-            Console.WriteLine(stats.HighestGrade);
-            Console.WriteLine(stats.LowestGrade);
+            WriteResult("Average", stats.AverageGrade);
+            WriteResult("Highest", (int)stats.HighestGrade);
+            WriteResult("Lowest", stats.LowestGrade);
 
+        }
+
+        static void WriteResult(string description, int result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine($"{description}: {result:F2}", description, result);
         }
     }
 }
