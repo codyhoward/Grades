08d1e278bdb8f797a6fc8b78683b15e041595db1
Cody <Cody@CSCPS>
2016-11-03 22:23:56 -0400

    Lots of changes in this bit -- S7C7-10 -- handling, chaining, use of finally, and cleaning up methods that were scattered everywhere.

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -14,21 +15,58 @@ namespace Grades
         {
 
             GradeBook book = new GradeBook();
-            //book.Name = null; 
-            //Will demonstrate use of throw exception
-            
-            book.AddGrade(91);
-            book.AddGrade(89.5f);
-            book.AddGrade(75);
-            book.WriteGrades(Console.Out);
 
+            GetBookName(book);
+            AddGrades(book);
+            SaveGrades(book);
+            WriteResults(book);
 
+        }
+
+        private static void WriteResults(GradeBook book)
+        {
             GradeStatistics stats = book.ComputeStatistics();
             WriteResult("Average", stats.AverageGrade);
             WriteResult("Highest", (int)stats.HighestGrade);
             WriteResult("Lowest", stats.LowestGrade);
             WriteResult(stats.Description, stats.LetterGrade);
+        }
 
+        private static void SaveGrades(GradeBook book)
+        {
+            using (StreamWriter outputFile = File.CreateText("grades.txt"))
+            {
+                book.WriteGrades(outputFile);
+            }
+        }
+
+        private static void AddGrades(GradeBook book)
+        {
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+        }
+
+        private static void GetBookName(GradeBook book)
+        {
+            try
+            {
+                Console.WriteLine("Please enter a name for this grade book.");
+                book.Name = Console.ReadLine();
+            }
+
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("Something went wrong!");
+            }
+            // Later I should implement a loop here to allow the user to keep trying until they get the name right... 
+
+            //book.Name = null; 
+            //Will demonstrate use of throw exception
         }
 
         static void WriteResult(string description, string result)
@@ -37,7 +75,7 @@ namespace Grades
         }
 
         static void WriteResult(string description, float result)
-        {   
+        {
             Console.WriteLine($"{description}: {result:F2}", description, result);
         }
 
